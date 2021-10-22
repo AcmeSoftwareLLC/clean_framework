e300a70644b31baa130f4fd32dd32841d863b9fa
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-22 19:42:05 +0545

    :white_check_mark: added tests for new router

--- lib/clean_framework.dart
+++ lib/clean_framework.dart
@@ -1,12 +1,12 @@
 library clean_framework;
 
 export 'package:clean_framework/src/app_providers_container.dart';
+export 'package:clean_framework/src/connectivity.dart';
+export 'package:clean_framework/src/defaults/network_service.dart';
 export 'package:clean_framework/src/feature_state/feature.dart';
 export 'package:clean_framework/src/feature_state/feature_mapper.dart';
 export 'package:clean_framework/src/feature_state/feature_state_provider.dart';
 export 'package:clean_framework/src/feature_state/feature_widget.dart';
 export 'package:clean_framework/src/logger.dart';
-export 'package:clean_framework/src/routing/routing.dart';
-export 'package:clean_framework/src/connectivity.dart';
-export 'package:clean_framework/src/defaults/network_service.dart';
+export 'package:clean_framework/src/routing/app_router.dart';
 export 'package:either_dart/either.dart';
