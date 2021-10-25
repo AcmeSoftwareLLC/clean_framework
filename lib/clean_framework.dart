cbd15aed6433defebd614ccf8b08ff0d0da1b3c5
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-25 20:41:20 +0545

    :recycle: removed old router APIs

--- lib/clean_framework.dart
+++ lib/clean_framework.dart
@@ -6,10 +6,6 @@ export 'package:clean_framework/src/feature_state/feature_mapper.dart';
 export 'package:clean_framework/src/feature_state/feature_state_provider.dart';
 export 'package:clean_framework/src/feature_state/feature_widget.dart';
 export 'package:clean_framework/src/logger.dart';
-
-//Deprecated
-export 'package:clean_framework/src/routing/routing.dart';
-//Replacement
 export 'package:clean_framework/src/routing/app_router.dart';
 
 export 'package:either_dart/either.dart';
