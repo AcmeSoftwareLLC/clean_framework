c5ea9912652bba167c4c1d9bced570ae51f20e48
sarbagyastha <sarbagyastha@gmail.com>
2022-05-12 07:20:49 +0545

    :alien: upgraded to Flutter 3

--- lib/src/providers/presenter.dart
+++ lib/src/providers/presenter.dart
@@ -47,7 +47,7 @@ class _PresenterState<V extends ViewModel, O extends Output, U extends UseCase>
   @override
   void initState() {
     super.initState();
-    SchedulerBinding.instance!.addPostFrameCallback((_) {
+    SchedulerBinding.instance.addPostFrameCallback((_) {
       widget.onLayoutReady(context, _useCase!);
     });
   }
