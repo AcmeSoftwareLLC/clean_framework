7010638f125ead249e5e405cb1d010f03b1189e1
sarbagyastha <sarbagyastha@gmail.com>
2022-06-06 16:29:22 +0545

    :sparkles: added `hook`

---
+++ lib/src/open_feature/src/hook/hook_hints.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+class HookHints<T extends Object> extends UnmodifiableMapBase<String, T> {
+  HookHints(this._map);
+
+  final Map<String, T> _map;
+
+  @override
+  Iterable<String> get keys => _map.keys;
+
+  @override
+  T? operator [](Object? key) => _map[key];
+}
