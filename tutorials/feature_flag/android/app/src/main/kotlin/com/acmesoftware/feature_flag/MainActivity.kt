9fd256daa5b9a18fb7abcdaf5a28bd33c39dd146
sarbagyastha <sarbagyastha@gmail.com>
2022-06-17 20:09:07 +0545

    :fire: deleted tutorials project

--- tutorials/feature_flag/android/app/src/main/kotlin/com/acmesoftware/feature_flag/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.acmesoftware.feature_flag
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tutorials/feature_flag/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:feature_flag/demo_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const DemoApp());
-}
