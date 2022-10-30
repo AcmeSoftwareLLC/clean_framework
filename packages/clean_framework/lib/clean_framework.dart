65c403a1a51d3f42f7ed564e4e99306da9ab02d8
sarbagyastha <sarbagyastha@gmail.com>
2022-10-30 17:53:38 +0545

    :truck: moved network logger to utilities

--- packages/clean_framework/lib/clean_framework.dart
+++ packages/clean_framework/lib/clean_framework.dart
@@ -2,7 +2,7 @@
 library clean_framework;
 
 export 'package:clean_framework/src/app_providers_container.dart';
-export 'package:clean_framework/src/defaults/providers/network_logger.dart';
+export 'package:clean_framework/src/utilities/network_logger.dart';
 export 'package:clean_framework/src/feature_state/feature.dart';
 export 'package:clean_framework/src/feature_state/feature_mapper.dart';
 export 'package:clean_framework/src/feature_state/feature_state_provider.dart';
