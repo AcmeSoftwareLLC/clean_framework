680b19c5d832ce9922855e5ab07be59a59d1f292
sarbagyastha <sarbagyastha@gmail.com>
2022-12-23 09:55:40 +0545

    :sparkles: added `OutputFilter.from` & `InputFilter.from`

--- packages/clean_framework/lib/src/providers/use_case/helpers/use_case_filter.dart
+++ packages/clean_framework/lib/src/providers/use_case/helpers/use_case_filter.dart
@@ -10,6 +10,14 @@ abstract class UseCaseFilter<E extends Entity> {}
 
 abstract class OutputFilter<E extends Entity, O extends Output>
     implements UseCaseFilter<E> {
+  const OutputFilter() : _transformer = null;
+
+  factory OutputFilter.from(O Function(E) transformer) = _OutputFilter<E, O>;
+
+  const OutputFilter._(this._transformer);
+
+  final O Function(E)? _transformer;
+
   MapEntry<Type, OutputBuilder<E>> get _entry => MapEntry(O, transform);
 
   @protected
@@ -18,10 +26,34 @@ abstract class OutputFilter<E extends Entity, O extends Output>
 
 abstract class InputFilter<E extends Entity, I extends Input>
     implements UseCaseFilter<E> {
+  const InputFilter() : _transformer = null;
+
+  factory InputFilter.from(E Function(E, I) transformer) = _InputFilter<E, I>;
+
+  const InputFilter._(this._transformer);
+
+  final E Function(E, I)? _transformer;
+
   MapEntry<Type, InputProcessor<E>> get _entry {
     return MapEntry(I, (i, e) => transform(e, i as I));
   }
 
   @protected
   E transform(E entity, I input);
+}
+
+class _OutputFilter<E extends Entity, O extends Output>
+    extends OutputFilter<E, O> {
+  const _OutputFilter(super.transformer) : super._();
+
+  @override
+  O transform(E entity) => _transformer!(entity);
+}
+
+class _InputFilter<E extends Entity, I extends Input>
+    extends InputFilter<E, I> {
+  const _InputFilter(super.transformer) : super._();
+
+  @override
+  E transform(E entity, I input) => _transformer!(entity, input);
 }
