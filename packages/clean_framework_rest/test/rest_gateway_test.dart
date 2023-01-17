763fcb2de7f58307ae331a152ab1d30808b34edd
sarbagyastha <sarbagyastha@gmail.com>
2023-01-17 19:21:19 +0545

    :sparkles: added new rest external interface

--- packages/clean_framework_rest/test/rest_gateway_test.dart
+++ packages/clean_framework_rest/test/rest_gateway_test.dart
@@ -1,6 +1,6 @@
 import 'package:clean_framework/clean_framework.dart';
 import 'package:clean_framework/clean_framework_legacy.dart';
-import 'package:clean_framework_rest/clean_framework_rest.dart';
+import 'package:clean_framework_rest/clean_framework_rest_legacy.dart';
 import 'package:clean_framework_test/clean_framework_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
