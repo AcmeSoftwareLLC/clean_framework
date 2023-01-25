61943e7a9a9810a2b398df0677a7dcc59d91f552
sarbagyastha <sarbagyastha@gmail.com>
2023-01-25 17:30:35 +0545

    :white_check_mark: added flow test

--- packages/clean_framework/example/lib/main.dart
+++ packages/clean_framework/example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:clean_framework/clean_framework.dart';
 import 'package:clean_framework_example/providers.dart';
 import 'package:clean_framework_example/routing/routes.dart';
+import 'package:clean_framework_example/widgets/cache_manager_scope.dart';
 import 'package:clean_framework_router/clean_framework_router.dart';
 import 'package:flutter/material.dart';
 
@@ -13,30 +14,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppProviderScope(
-      externalInterfaceProviders: [
-        pokemonExternalInterfaceProvider,
-      ],
-      child: AppRouterScope(
-        create: PokeRouter.new,
-        builder: (context) {
-          return MaterialApp.router(
-            title: 'Clean Framework Example',
-            routerConfig: context.router.config,
-            theme: ThemeData.from(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-              useMaterial3: true,
-            ),
-            darkTheme: ThemeData.from(
-              colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.green,
-                brightness: Brightness.dark,
+    return CacheManagerScope(
+      child: AppProviderScope(
+        externalInterfaceProviders: [
+          pokemonExternalInterfaceProvider,
+        ],
+        child: AppRouterScope(
+          create: PokeRouter.new,
+          builder: (context) {
+            return MaterialApp.router(
+              title: 'Clean Framework Example',
+              routerConfig: context.router.config,
+              theme: ThemeData.from(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+                useMaterial3: true,
               ),
-              useMaterial3: true,
-            ),
-            themeMode: ThemeMode.dark,
-          );
-        },
+              darkTheme: ThemeData.from(
+                colorScheme: ColorScheme.fromSeed(
+                  seedColor: Colors.green,
+                  brightness: Brightness.dark,
+                ),
+                useMaterial3: true,
+              ),
+              themeMode: ThemeMode.dark,
+            );
+          },
+        ),
       ),
     );
   }
